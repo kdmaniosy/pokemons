96fced5a0e4096698d9f8f3433fb0851b626cb9d
kdmaniosy <kdmaniosy@ufpso.edu.co>
2024-06-19 18:43:20 -0500

    Api Pokemons

---
+++ src/components/vue/Pokemon.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+const pokemons = ref([])
+
+function loadPokemons() {
+    fetch('https://pokeapi.co/api/v2/pokemon')
+        .then(function (response){
+            return response.json()
+        })
+        .then(function (data) {
+            //console.log(data.results)
+            for(let i = 0; i < data.results.length; i++){
+                console.log(i)
+                pokemons.value.push(data.results[i])
+            }
+        })
+    
+}
+
+
+</script>
+
+<template>
+    <div v-for="(pok, index) in pokemons" :key="index" class="bg-purple-300 mb-1">
+        {{ pok.name }}
+    </div>
+    <button @click="loadPokemons">Cargar pokemons</button>
+
+
+</template>
